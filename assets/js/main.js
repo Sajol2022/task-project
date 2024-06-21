850bf767a5d2b293fb354e22a235a781ad8876fb
Sajol2022 <sajolsarker5789@gmail.com>
2024-06-21 13:47:59 +0600

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 (function ($) {
     "use strict";
     // Banner Slider
-		var swiper = new Swiper(".banner_slider", {
-			slidesPerView: 1,
-			loop: true,
-            speed: 2000,
-			autoplay: {
-				delay: 4000,
-				reverseDirection: false,
-				disableOnInteraction: false,
-			},
-            pagination: {
-              el: ".banner_pagination_dots",
-              clickable: true,
-            },
-		});
+	var swiper = new Swiper(".banner_slider", {
+		slidesPerView: 1,
+		loop: true,
+		speed: 2000,
+		autoplay: {
+			delay: 4000,
+			reverseDirection: false,
+			disableOnInteraction: false,
+		},
+		pagination: {
+		  el: ".banner_pagination_dots",
+		  clickable: true,
+		},
+	});
 })(jQuery);
